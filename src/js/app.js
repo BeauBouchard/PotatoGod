779320e847e4cc60e0da3393054001ed1bc0d7b8
Beau Bouchard <beau@beaubouchard.com>
2013-06-17 13:37:00 -0400

    Create app.js

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+/*
+ * Title: app.js
+ * Description: app 
+ * Author: Beau Bouchard (@beaubouchard)
+ */
+ 
+ 
+var canvas = document.getElementByID("canvas");
+var content = canvas.getContext("2d"); 
+
+//This is the main game loop for running.
+function main(){
+
+}
+ 
+ function checkCollisions() {
+ 
+ }
