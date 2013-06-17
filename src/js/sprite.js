caf213ad28a91c173ef264a9c094b34780c6bdc1
Beau Bouchard <beau@beaubouchard.com>
2013-06-17 14:38:53 -0300

    creating sprite.js, which will be the class for most of the sprites used during game play. Testing out some new OOP techniques so maybe changing a lot

---
+++ src/js/sprite.js
@@ -0,0 +1,48 @@
+/*
+ * Title: sprite.js
+ * Description: Trying out a new way to load, and manage sprite objects
+ * Author: Beau Bouchard (@beaubouchard)
+ */
+ 
+ //Register Namespace
+ var Sprite = Sprite||{};
+ 
+ //Constructor for Characters (moving sprites, NPC, or Player)
+Sprite.Character = function Sprite_Character(inc_source,inc_size,inc_frames){
+  this.source = inc_source;
+	this.size = inc_size;
+	this.frames = inc_frames
+
+}
+//Constructor for Enviroment Objects (Plants, buildings, fences)
+ Sprite.Enviroment = function Sprite_Enviroment(inc_source,inc_size){
+	this.size = inc_size;
+}
+
+//Character functionality
+function Sprite.Character.prototype() {
+	
+	//Render: function Sprite_Character_Render(inc_canvasID,inc_x,inc_y){
+		//var mcanvas = document.getElementByID(inc_canvasID);
+		//var mcontent = canvas.getContext("2d"); 
+		//I may just pass in the context to render
+		
+		//this.x = inc_x ||canvas.width/2
+		//this.y = inc_y || canvas.height/2;
+	//}
+	
+	//Walk: function Sprite_Character_Walk(inc_canvasID,fromx,fromy,tox,toy){}
+	
+	/*Plant: Function*/
+	
+	//if sprite is facing different directions, use different sprites
+	
+}
+
+//static variables
+Sprite.Character.Viewdistance = 5; 
+
+
+
+
+
