ca3f1965f9e926b3e9c27038c8443b633a6f0f1a
Beau Bouchard <beau@beaubouchard.com>
2013-06-19 17:07:39 -0300

    added score to game

--- src/js/app.js
+++ src/js/app.js
@@ -8,22 +8,36 @@
 var canvas = document.getElementByID("canvas");
 var content = canvas.getContext("2d"); 
 
+// Game Object
+// Controls gamestate,
 function game(){
     this.state = 0;
- 
+    this.score = 0;
 }
 
 game.prototype = {
- getinfo: function() {
-  return this.state;
- }
- getGameState: function() {
-  return this.state;
- }
- advanceGameState: function () {
-  this.state ++;
- }
- 
+   getinfo: function() {
+      return this.state;
+   }
+   getGameState: function() {
+      return this.state;
+   }
+   getGameScore: function() {
+      return this.score;
+   }
+   advanceGameState: function () {
+      this.state ++;
+   }
+   reset: function () {
+      this.state = 0;
+      this.score = 0;
+      this.firstRun();
+   }
+   firstRun: function () {
+    //game just started.
+    //reset score to 0, just to make sure
+    this.score = 0;
+   }
 }
 
 //This is the main game loop for running.
