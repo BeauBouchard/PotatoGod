84be17ae5751154870431bd04d103917e8d101c5
Beau Bouchard <beau@beaubouchard.com>
2013-06-19 17:00:16 -0300

    added some code to create game objects, which will control the gamestate begining and end functionality

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,24 @@
 var canvas = document.getElementByID("canvas");
 var content = canvas.getContext("2d"); 
 
+function game(){
+    this.state = 0;
+ 
+}
+
+game.prototype = {
+ getinfo: function() {
+  return this.state;
+ }
+ getGameState: function() {
+  return this.state;
+ }
+ advanceGameState: function () {
+  this.state ++;
+ }
+ 
+}
+
 //This is the main game loop for running.
 function main(){
 
