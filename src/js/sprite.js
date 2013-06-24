53d36077c325e2a95e8e175ba82ef08d0fd33ca4
Beau Bouchard <beau@beaubouchard.com>
2013-06-24 14:00:31 -0300

    Update sprite.js

--- src/js/sprite.js
+++ src/js/sprite.js
@@ -9,19 +9,32 @@
  
  //Constructor for Characters (moving sprites, NPC, or Player)
 Sprite.Character = function Sprite_Character(inc_source,inc_size,inc_frames){
-  this.source = inc_source;
+  	this.source = inc_source;
 	this.size = inc_size;
 	this.frames = inc_frames
 
 }
 //Constructor for Enviroment Objects (Plants, buildings, fences)
- Sprite.Enviroment = function Sprite_Enviroment(inc_source,inc_size){
-	this.size = inc_size;
+ Sprite.Enviroment = function Sprite_Enviroment(){
+
 }
 
 //Character functionality
-function Sprite.Character.prototype() {
+function Sprite.Character.prototype(inc_source,inc_size) {
+	this.x = 0;
+	this.y = 0;
+	this.source = inc_source;
+	this.size = inc_size;
 	
+	
+	initialize: function (inc_x,inc_y) {
+		this.x = inc_x;
+		this.y = inc_y;
+	}
+	toString: function () {
+		
+		console.log("X:"+this.x+"\nY:" + this.y);
+	}
 	//Render: function Sprite_Character_Render(inc_canvasID,inc_x,inc_y){
 		//var mcanvas = document.getElementByID(inc_canvasID);
 		//var mcontent = canvas.getContext("2d"); 
